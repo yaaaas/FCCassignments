7f98596481c5e985dcb61d9717268ac000726483
Ivan Leo <ivanleo97@gmail.com>
2017-07-25 14:15:29 +0800

    Update Return_Largest_Numbers.js

--- frontend/Return_Largest_Numbers.js
+++ frontend/Return_Largest_Numbers.js
@@ -6,13 +6,18 @@
 
 function largestOfFour(arr) {
   
+ //Step 2 is initiated with the map function iterating over each individual nested array
   var largest = arr.map(function(nums){
+  
+  // Step 3 is executed using the reduce function on a nested array. Numbers are compared starting from the last number in the array 
+     to determine which is the larger one. The larger one is returned and compared with the next number in line.
   var long = nums.reduce(function(a, b) {
     return Math.max(a, b);
 });
   
+  //This value is then returned and stored in the largest array.
   return long;  
   });
-  // You can do this!
+  // We then return the largest array which contains all of the largest numbers in the original array passed in.
   return largest;
 }
